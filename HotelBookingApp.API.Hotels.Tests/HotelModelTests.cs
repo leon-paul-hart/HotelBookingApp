38a7d7d5eaccfb675d8f952c7867fe7c05d807ce
Leon-Paul Hart <leon.paul.hart@live.com>
2021-11-19 00:06:08 +0000

    Started unit testing of Hotel API models, initial Null test checks for new Hotel, instantiated an empty List<Room> to avoid possible null reference.

---
+++ HotelBookingApp.API.Hotels.Tests/HotelModelTests.cs
@@ -0,0 +1,88 @@
+﻿using HotelBookingApp.API.Hotels.Models;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBookingApp.API.Hotels.Tests
+{
+    [TestClass]
+    public class HotelModelTests
+    {
+        [TestMethod]
+        public void NewHotelModel_NotNull()
+        {
+            // Arrange
+            Hotel hotel = new();
+
+            // Act
+
+            // Assert
+            Assert.IsNotNull(hotel);
+        }
+
+        [TestMethod]
+        public void NewHotelModel_ID_NotNull()
+        {
+            // Arrange
+            Hotel hotel = new();
+
+            // Act
+
+            // Assert
+            Assert.IsNotNull(hotel.HotelID);
+        }
+
+        [TestMethod]
+        public void NewHotelModel_Name_NotNull()
+        {
+            // Arrange
+            Hotel hotel = new();
+
+            // Act
+
+            // Assert
+            Assert.IsNotNull(hotel.HotelName);
+        }
+
+        [TestMethod]
+        public void NewHotelModel_StreetAddress_NotNull()
+        {
+            // Arrange
+            Hotel hotel = new();
+
+            // Act
+
+            // Assert
+            Assert.IsNotNull(hotel.HotelStreetAddress);
+        }
+
+        [TestMethod]
+        public void NewHotelModel_Postcode_NotNull()
+        {
+            // Arrange
+            Hotel hotel = new();
+
+            // Act
+
+            // Assert
+            Assert.IsNotNull(hotel.HotelPostcode);
+        }
+
+        [TestMethod]
+        public void NewHotelModel_RoomList_NotNull()
+        {
+            // Arrange
+            Hotel hotel = new();
+
+            // Act
+
+            // Assert
+            Assert.IsNotNull(hotel.HotelRooms);
+        }
+    }
+}
