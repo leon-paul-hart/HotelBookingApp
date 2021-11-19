57cc327f4dec1dc81a51776d313b8f391b2f835a
Leon-Paul Hart <leon.paul.hart@live.com>
2021-11-19 08:13:08 +0000

    Added room model tests

---
+++ HotelBookingApp.API.Hotels.Tests/RoomModelTests.cs
@@ -0,0 +1,70 @@
+﻿using HotelBookingApp.API.Hotels.Models;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RoomBookingApp.API.Rooms.Tests
+{
+    [TestClass]
+    public class RoomModelTests
+    {
+        [TestMethod]
+        public void NewRoomModel_NotNull()
+        {
+            // Arrange
+            Room room = new();
+
+            // Act
+
+            // Assert
+            Assert.IsNotNull(room);
+        }
+
+        [TestMethod]
+        public void NewRoomModel_ID_NotNull()
+        {
+            // Arrange
+            Room room = new();
+
+            // Act
+
+            // Assert
+            Assert.IsNotNull(room.RoomID);
+        }
+
+        [TestMethod]
+        public void NewRoomModel_Number_NotNull()
+        {
+            // Arrange
+            Room room = new();
+
+            // Act
+
+            // Assert
+            Assert.IsNotNull(room.RoomNumber);
+        }
+
+        [TestMethod]
+        public void NewRoomModel_RoomType_NotNull()
+        {
+            // Arrange
+            Room room = new();
+
+            // Act
+
+            // Assert
+            Assert.IsNotNull(room.RoomType);
+        }
+
+        [TestMethod]
+        public void NewRoomModel_RoomAvailable_NotNull()
+        {
+            // Arrange
+            Room room = new();
+
+            // Act
+
+            // Assert
+            Assert.IsNotNull(room.RoomAvailable);
+        }
+    }
+}
