22c36f6d487b97176c313c3e56cec70b376a2546
Leon-Paul Hart <leon.paul.hart@live.com>
2021-11-21 15:51:52 +0000

    Created Business Logic library with APIHelper class

---
+++ HotelBookingApp.Libraries.BusinessLogic/ApiHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http.Headers;
+
+namespace HotelBookingApp.Libraries.BusinessLogic
+{
+    public static class ApiHelper
+    {
+        public static HttpClient ApiClient { get; set; } = new HttpClient();
+
+        public static void InitialiseClient()
+        {
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
