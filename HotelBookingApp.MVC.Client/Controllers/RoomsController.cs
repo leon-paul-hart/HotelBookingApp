486147d73d1a536aefc2fde71d5ca8a42977256c
Leon-Paul Hart <leon.paul.hart@live.com>
2021-11-26 10:39:45 +0000

    Added base Room model/view/controller

---
+++ HotelBookingApp.MVC.Client/Controllers/RoomsController.cs
@@ -0,0 +1,116 @@
+﻿using HotelBookingApp.MVC.Client.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Newtonsoft.Json;
+
+namespace HotelBookingApp.MVC.Client.Controllers
+{
+    public class RoomsController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public RoomsController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
+        }
+
+        // GET: RoomsController
+        [HttpGet]
+        public async Task<ActionResult> IndexAsync()
+        {
+            HttpClient httpClient = _httpClientFactory.CreateClient("HotelAPI");
+
+            HttpResponseMessage? response = await httpClient.GetAsync("rooms");
+
+            List<RoomViewModel> rooms = new List<RoomViewModel>();
+
+            try
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    string? validResponse = response.Content.ReadAsStringAsync().Result;
+
+                    rooms = JsonConvert.DeserializeObject<List<RoomViewModel>>(validResponse);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+
+                return View("Error", ex);
+            }
+
+            return View(rooms);
+        }
+
+        // GET: RoomsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: RoomsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RoomsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(IndexAsync));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RoomsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: RoomsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(IndexAsync));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RoomsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: RoomsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(IndexAsync));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- HotelBookingApp.MVC.Client/Models/RoomViewModel.cs
+++ HotelBookingApp.MVC.Client/Models/RoomViewModel.cs
@@ -2,5 +2,14 @@
 {
     public class RoomViewModel
     {
+        public int RoomID { get; set; }
+
+        public int RoomNumber { get; set; }
+
+        public string RoomType { get; set; } = string.Empty;
+
+        public bool RoomAvailable { get; set; } = false;
+
+        public int HotelID { get; set; }
     }
 }
